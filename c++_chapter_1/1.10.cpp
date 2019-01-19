c3196c2c5f6bee1c81bf0bbd239b1b42af23119c
563816752 <33140653+563816752@users.noreply.github.com>
2019-01-19 21:47:47 +0800

    test1

---
+++ c++_chapter_1/1.10.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    int i=10;
+    while(i>0)
+    {
+        cout<<i<<endl;
+        i--;
+    }
+    cout<<"the end"<<endl;
+    return 0;
+}
