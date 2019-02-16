15745eff98119a6e3aa97f0c7b353ab3aab87212
563816752 <563816752@qq.com>
2019-02-16 12:44:50 +0800

    666

--- c++_chapter_2/struct.cpp
+++ c++_chapter_2/struct.cpp
@@ -2,12 +2,13 @@
 using namespace std;
 struct sale_data{
     double price;
-    string name;
-};
+    string name="NULL";
+}sale;
 int main()
 {
     sale_data data1;
+    sale.name;
     data1.name="1111";
-    cout<<data1.name<<endl;
+    cout<<data1.name<<"  "<<sale.name<<endl;
     return 0;
 }
